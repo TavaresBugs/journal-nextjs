e8675de8939d9010b6c0f2bc378101c1324fab4c
TavaresBugs <jhontavares2025@gmail.com>
2025-12-19 10:01:19 -0300

    fix(csp): allow blob scripts in development

--- next.config.mjs
+++ next.config.mjs
@@ -50,7 +50,7 @@ const nextConfig = {
             key: "Content-Security-Policy",
             value: [
               "default-src 'self'",
-              "script-src 'self' 'unsafe-inline' 'unsafe-eval'",
+              "script-src 'self' 'unsafe-inline' 'unsafe-eval' blob:",
               "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com",
               "font-src 'self' https://fonts.gstatic.com",
               "img-src 'self' data: https: blob:",
